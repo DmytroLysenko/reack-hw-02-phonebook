3a4c5cd37884781f94fc3b231b330d6dbbd6624d
DmytroLysenko <DmytroLysenko@users.noreply.github.com>
2020-09-25 11:00:56 +0300

    Updates

---
+++ precache-manifest.355685a5169e470909bf568386dd111f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a65dbaf4a9825cc24d7d58e1d4eeacca",
+    "url": "/react-hw-02-phonebook/index.html"
+  },
+  {
+    "revision": "ec27e9cf8d7132efbb03",
+    "url": "/react-hw-02-phonebook/static/css/main.ac6f597d.chunk.css"
+  },
+  {
+    "revision": "12504ca1f0b967710530",
+    "url": "/react-hw-02-phonebook/static/js/2.81e873fd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-hw-02-phonebook/static/js/2.81e873fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec27e9cf8d7132efbb03",
+    "url": "/react-hw-02-phonebook/static/js/main.c05ba6c6.chunk.js"
+  },
+  {
+    "revision": "6f482f3e5b3d66793c08",
+    "url": "/react-hw-02-phonebook/static/js/runtime-main.2c5ed82f.js"
+  }
+]);
